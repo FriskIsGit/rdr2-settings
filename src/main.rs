d3006f7460c4978cc1900061a8c473bffdc25d10
FriskIsGit <friskisgit@users.noreply.github.com>
2024-03-06 21:00:24 +0100

    added xml serializing and many new options, including resolutions selection

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ use crate::inputs::windows::{read_key, clear_console};
 
 #[cfg(all(unix))]
 use crate::inputs::unix::{read_key, clear_console};
+use crate::settings::SettingType::{Level, Multiplier, OnHalfOff, OnOff, Slider};
 
 mod settings;
 mod inputs;
 
-const MIN_VRAM: usize = 1537;  // All minimal settings (1024 x 768)
+// Highest low [test on minimal settings (1024 x 768)] claimed=1537
+const MIN_VRAM: usize = 1670;
 const RECOMMENDED_VRAM: usize = 6144;
 const PADDING: usize = 40;
 
@@ -32,12 +34,16 @@ fn main() {
 fn start_console(vram_available_mbs: usize) {
     let mut settings = settings::get_settings();
     let capacity = settings_string_capacity(&settings);
+    // hacky code since both settings are dependent on each other
+    let width_index = settings::get_setting_index_by_tag(&settings, "screenWidthWindowed").unwrap();
+    let height_index = settings::get_setting_index_by_tag(&settings, "screenHeightWindowed").unwrap();
+
     let mut index = 0;
     let mut cycle_settings = true;
     let mut vram_used = MIN_VRAM as f64;
     while cycle_settings {
         let mut format = String::with_capacity(capacity);
-        format.push_str(&format!("==== VRAM USAGE {vram_used} / {vram_available_mbs} ====\n"));
+        format.push_str(&format!("==== VRAM USAGE {vram_used:.0} / {vram_available_mbs} ====\n"));
 
         for (i, setting) in settings.iter().enumerate() {
             if i == index {
@@ -55,7 +61,7 @@ fn start_console(vram_available_mbs: usize) {
 
         let key = read_key();
         match key {
-            KeyCode::Backspace  => {
+            KeyCode::Backspace => {
                 cycle_settings = false;
             }
             KeyCode::ArrowUp | KeyCode::Char('w') | KeyCode::Char('W') => {
@@ -69,25 +75,30 @@ fn start_console(vram_available_mbs: usize) {
                 }
             }
             KeyCode::ArrowLeft | KeyCode::Char('a') | KeyCode::Char('A') => {
-                let setting = &mut settings[index];
-                match &mut setting.setting_type {
-                    SettingType::Level(selected_index, _, vram_levels) => {
+                let Slider(width, _, _) = settings[width_index].setting_type else {
+                    panic!("Width not found?");
+                };
+                let Slider(height, _, _) = settings[height_index].setting_type else {
+                    panic!("Height not found?");
+                };
+                match &mut settings[index].setting_type {
+                    Level(selected_index, _, vram_levels) => {
                         if *selected_index > 0 {
                             *selected_index -= 1;
                             vram_used -= vram_levels[*selected_index] as f64;
                         }
                     }
-                    SettingType::OnOff(enabled) => {
+                    OnOff(enabled) => {
                         *enabled = !*enabled;
                     }
-                    SettingType::Multiplier(value, _) => {
+                    Multiplier(value, _) => {
                         if *value <= 2 {
                             *value = 0;
                         } else {
                             *value /= 2;
                         }
                     }
-                    SettingType::OnHalfOff(value) => {
+                    OnHalfOff(value) => {
                         // OFF ON HALF
                         if *value == 0 {
                             *value = 2;
@@ -97,28 +108,46 @@ fn start_console(vram_available_mbs: usize) {
                             *value = 1;
                         }
                     }
+                    Slider(value, jump, horizontal) => {
+                        let jmp = *jump;
+                        if *value < jmp * 2 {
+                            continue;
+                        }
+                        *value -= jmp;
+                        let pixel_diff = match *horizontal {
+                            true => (height * jmp) as f64,
+                            false => (width * jmp) as f64
+                        };
+                        let mb_diff = pixel_diff / settings::PIXELS_PER_1MB_VRAM;
+                        vram_used -= mb_diff;
+                    }
                 }
             }
             KeyCode::ArrowRight | KeyCode::Char('d') | KeyCode::Char('D') => {
-                let setting = &mut settings[index];
-                match &mut setting.setting_type {
-                    SettingType::Level(selected_index, selectable, vram_levels) => {
+                let Slider(width, _, _) = settings[width_index].setting_type else {
+                    panic!("Width not found?");
+                };
+                let Slider(height, _, _) = settings[height_index].setting_type else {
+                    panic!("Height not found?");
+                };
+                match &mut settings[index].setting_type {
+                    Level(selected_index, selectable, vram_levels) => {
                         if *selected_index + 1 < selectable.len() {
                             *selected_index += 1;
                             vram_used += vram_levels[*selected_index - 1] as f64;
                         }
                     }
-                    SettingType::OnOff(enabled) => {
+                    OnOff(enabled) => {
                         *enabled = !*enabled;
                     }
-                    SettingType::Multiplier(value, max_factor) => {
+                    Multiplier(value, max_factor) => {
                         if *value == 0 {
                             *value = 2;
                         } else if value < max_factor && *value >= 2 {
                             *value *= 2;
                         }
                     }
-                    SettingType::OnHalfOff(value) => {
+                    OnHalfOff(value) => {
                         // OFF ON HALF
                         if *value == 0 {
                             *value = 1;
@@ -128,12 +157,24 @@ fn start_console(vram_available_mbs: usize) {
                             *value = 0;
                         }
                     }
+                    Slider(value, jump, horizontal) => {
+                        let jmp = *jump;
+                        *value += jmp;
+                        let pixel_diff = match *horizontal {
+                            true => (height * jmp) as f64,
+                            false => (width * jmp) as f64
+                        };
+                        let mb_diff = pixel_diff / settings::PIXELS_PER_1MB_VRAM;
+                        vram_used += mb_diff;
+                        //add vram reduction logic
+                    }
                 }
             }
             KeyCode::Enter => {
-                cycle_settings = false;
+                println!("Committing write");
+                settings::commit_xml_write(settings);
                 break;
-            },
+            }
             _ => {}
         }
     }
@@ -141,18 +182,18 @@ fn start_console(vram_available_mbs: usize) {
 
 fn append_setting_type(format: &mut String, setting_type: &SettingType) {
     match setting_type {
-        SettingType::Level(selected_index, selectable, vram_levels) => {
+        Level(selected_index, selectable, _) => {
             let option = &selectable[*selected_index];
             format.push_str(&option.nice_name);
         }
-        SettingType::OnOff(enabled) => {
+        OnOff(enabled) => {
             if *enabled {
                 format.push_str("ON");
             } else {
                 format.push_str("OFF");
             }
         }
-        SettingType::Multiplier(value, _) => {
+        Multiplier(value, _) => {
             if *value == 0 {
                 format.push_str("OFF");
             } else {
@@ -160,7 +201,7 @@ fn append_setting_type(format: &mut String, setting_type: &SettingType) {
                 format.push_str(&value.to_string());
             }
         }
-        SettingType::OnHalfOff(value) => {
+        OnHalfOff(value) => {
             if *value == 0 {
                 format.push_str("OFF");
             } else if *value == 1 {
@@ -168,6 +209,9 @@ fn append_setting_type(format: &mut String, setting_type: &SettingType) {
             } else if *value == 2 {
                 format.push_str("HALF");
             }
+        }
+        Slider(value, _, _) => {
+            format.push_str(&value.to_string());
         }
     }
 
--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,8 @@
-use crate::settings::XMLSection::{Graphics, Video};
+use std::fs::File;
+use std::io::Write;
+use xmlwriter::{Options, XmlWriter};
+use crate::settings::SettingType::{Level, Multiplier, OnHalfOff, OnOff, Slider};
+use crate::settings::XMLSection::{AdvancedGraphics, Graphics, Video};
 
 /**
 Attribute()
@@ -9,14 +13,18 @@ type SelectedIndex = usize;
 type Value = usize;
 type MaxFactor = usize;
 type Enabled = bool;
+type Jump = usize;
+type IsHorizontal = bool;
 
 pub enum SettingType {
     Level(SelectedIndex, Vec<Selectable>, VRamLevels),
     OnOff(Enabled),
     OnHalfOff(Value), // off=0, on=1, half=2
     Multiplier(Value, MaxFactor),
+    Slider(Value, Jump, bool),
 }
 
+#[derive(PartialEq)]
 pub enum XMLSection {
     Graphics,
     Video,
@@ -29,6 +37,8 @@ pub struct Setting {
     pub setting_type: SettingType,
     pub section: XMLSection,
 }
+
+pub const PIXELS_PER_1MB_VRAM: f64 = 5155.0;
 
 impl Setting {
     pub fn off_medium_high(section: XMLSection, tag: &str, nice_name: &str, step1: usize, step2: usize) -> Self {
@@ -92,11 +102,11 @@ impl Setting {
             section,
         }
     }
-    pub fn resolutions(section: XMLSection, nice_name: &str) -> Self {
+    pub fn screen_pixels(section: XMLSection, tag: &str, nice_name: &str, curr_val: usize, jump: usize, is_horizontal: bool) -> Self {
         Self {
-            nice_name: "".to_string(),
-            tag: "".to_string(),
-            setting_type: SettingType::OnOff(false),
+            nice_name: nice_name.into(),
+            tag: tag.into(),
+            setting_type: SettingType::Slider(curr_val, jump, is_horizontal),
             section,
         }
     }
@@ -171,8 +181,209 @@ pub fn get_settings() -> Vec<Setting> {
 
         Setting::on_off(Video, "tripleBuffered", "Triple Buffering"),
         Setting::on_half_off(Video, "vSync", "VSync"),
-        Setting::api_options(Video, "API", "Graphical API"),
-        // Setting::resolutions(Video, "API", "Graphical API"), not very soon
+        Setting::screen_pixels(Video, "screenWidthWindowed", "Width Pixels", 1024, 8, true),
+        Setting::screen_pixels(Video, "screenHeightWindowed", "Height Pixels", 768, 4, false),
+        Setting::api_options(AdvancedGraphics, "API", "Graphical API"),
+        Setting::on_off(AdvancedGraphics, "motionBlur", "Motion Blur"),
+        Setting::on_off(AdvancedGraphics, "waterReflectionSSR", "Water Reflection SSR"),
+        Setting::low_medium_high(AdvancedGraphics, "waterRefractionQuality", "Water Refraction Quality", 2, 5),
+        Setting::low_medium_high(AdvancedGraphics, "waterReflectionQuality", "Water Reflection Quality", 3, 15),
+        Setting::low_medium_high_ultra(AdvancedGraphics, "particleLightingQuality", "Particle Lighting Quality", 18, 0, 0),
+        Setting::off_medium_high_ultra(AdvancedGraphics, "shadowSoftShadows", "Soft Shadows", 0, 0, 0),
     ];
     settings
+}
+
+pub fn get_setting_index_by_tag(settings: &[Setting], tag: &str) -> Option<usize> {
+    for (i, setting) in settings.iter().enumerate() {
+        if setting.tag == tag {
+            return Some(i);
+        }
+    }
+    return None;
+}
+
+pub fn commit_xml_write(settings: Vec<Setting>) {
+    let opt = Options {
+        use_single_quote: false, // RDR2 has double quote
+        ..Options::default()
+    };
+    let mut xml = XmlWriter::new(opt);
+    xml.write_declaration();
+
+    xml.start_element("rage__fwuiSystemSettingsCollection");
+
+    xml.start_element("version");
+    xml.write_attribute("value", "37");
+    xml.end_element();
+
+    xml.start_element("configSource");
+    xml.write_text("kSettingsConfig_Auto");
+    xml.end_element();
+
+    xml.start_element("graphics");
+    write_default_graphics(&mut xml);
+    for setting in settings.iter() {
+        if setting.section != Graphics {
+            continue;
+        }
+        xml.start_element(&setting.tag);
+        match &setting.setting_type {
+            Level(index, selectables, _) => {
+                xml.write_text(&selectables[*index].config_name);
+            }
+            OnOff(on) => {
+                let boolean = if *on { "true" } else { "false" };
+                xml.write_attribute("value", boolean);
+            }
+            Multiplier(value, _) => {
+                xml.write_attribute("value", &value.to_string());
+            }
+            Slider(_, _, _) => {}
+            OnHalfOff(_) => {}
+        }
+        xml.end_element();
+    }
+    xml.end_element();
+
+    xml.start_element("video");
+    write_default_video(&mut xml);
+    for setting in settings.iter() {
+        if setting.section != Video {
+            continue;
+        }
+        xml.start_element(&setting.tag);
+        match &setting.setting_type {
+            Level(index, selectables, _) => xml.write_text(&selectables[*index].config_name),
+            OnOff(on) => {
+                let boolean = if *on { "true" } else { "false" };
+                xml.write_attribute("value", boolean);
+            }
+            Multiplier(value, _) => xml.write_attribute("value", &value),
+            OnHalfOff(vsync) => xml.write_attribute("value", vsync),
+            Slider(pixels, _, _) => xml.write_attribute("value", pixels)
+        }
+        xml.end_element();
+    }
+    xml.end_element();
+
+    xml.start_element("advancedGraphics");
+    write_default_advanced_graphics(&mut xml);
+    for setting in settings.iter() {
+        if setting.section != AdvancedGraphics {
+            continue;
+        }
+        xml.start_element(&setting.tag);
+        match &setting.setting_type {
+            Level(index, selectables, _) => xml.write_text(&selectables[*index].config_name),
+            OnOff(on) => {
+                let boolean = if *on { "true" } else { "false" };
+                xml.write_attribute("value", boolean);
+            }
+            OnHalfOff(_) => {}
+            Multiplier(value, _) => {}
+            Slider(pixels, _, _) => xml.write_attribute("value", pixels),
+        }
+        xml.end_element();
+    }
+    xml.end_element();
+
+    xml.start_element("videoCardDescription");
+    xml.write_text("GPU CARD NAME");
+    xml.end_element();
+
+    let content = xml.end_document();
+    let mut file = File::create("system.xml").expect("Couldn't create file");
+    file.write_all(content.as_bytes()).expect("Failed to write to file");
+}
+
+fn write_default_graphics(xml: &mut XmlWriter) {
+    write_element("dlssIndex", "0", xml);
+    write_element("dlssQuality", "5", xml);
+    write_element("graphicsQualityPreset", "0.5", xml);
+    write_element("hdr", "true", xml);
+    write_element("hdrIntensity", "100", xml);
+    write_element("hdrPeakBrightness", "1000", xml);
+    write_element("hdrFilmicMode", "true", xml);
+    write_element("gamma", "15", xml);
+    write_element("hdrSettingsMigrated", "true", xml);
+}
+
+fn write_default_video(xml: &mut XmlWriter) {
+
+    write_element("adapterIndex", "0", xml); // output adapter
+    write_element("outputIndex", "0", xml); // output monitor
+
+    write_element("resolutionIndexWindowed", "0", xml);
+    write_element("resolutionIndex", "1", xml);
+
+    write_element("screenWidth", "1240", xml);
+    write_element("screenHeight", "720", xml);
+
+    write_element("refreshRateIndex", "0", xml);
+    write_element("refreshRateNumerator", "60", xml);
+    write_element("refreshRateDenominator", "1", xml);
+
+    write_element("windowed", "2", xml);
+    write_element("pauseOnFocusLoss", "false", xml); // make selectable
+    write_element("constrainMousePointer", "false", xml); // make selectable
+}
+
+fn write_default_advanced_graphics(xml: &mut XmlWriter) {
+    write_element("locked", "false", xml);
+    write_element("asyncComputeEnabled", "false", xml);
+    write_element("transferQueuesEnabled", "true", xml);
+    write_element("motionBlurLimit", "16.0", xml);
+    write_element("waterSimulationQuality", "3", xml); // make selectable
+    write_text_element("waterLightingQuality", "kSettingLevel_Ultra", xml); //make selectable
+    write_text_element("furDisplayQuality", "kSettingLevel_Medium", xml); // make selectable
+    write_element("maxTexUpgradesPerFrame", "5", xml);
+    // check
+    write_text_element("shadowGrassShadows", "kSettingLevel_High", xml);
+    write_element("shadowParticleShadows", "true", xml);
+    write_element("shadowLongShadows", "true", xml);
+    write_element("directionalShadowsAlpha", "false", xml);
+    write_element("worldHeightShadowQuality", "1.0", xml);
+    write_element("directionalScreenSpaceShadowQuality", "1.0", xml);
+    write_element("ambientMaskVolumesHighPrecision", "true", xml);
+    write_text_element("scatteringVolumeQuality", "kSettingLevel_High", xml);
+    write_text_element("volumetricsRaymarchQuality", "kSettingLevel_High", xml);
+    write_text_element("volumetricsLightingQuality", "kSettingLevel_High", xml);
+    write_element("volumetricsRaymarchResolutionUnclamped", "true", xml);
+    write_text_element("terrainShadowQuality", "kSettingLevel_Ultra", xml);
+    write_element("damageModelsDisabled", "false", xml); //interesting?
+    write_text_element("decalQuality", "kSettingLevel_High", xml);
+    write_element("ssaoFullScreenEnabled", "false", xml);
+    write_element("ssaoType", "0", xml);
+    write_element("ssdoSampleCount", "4", xml);
+    write_element("ssdoUseDualRadii", "false", xml);
+    write_text_element("ssdoResolution", "kSettingLevel_Low", xml);
+    write_element("ssdoTAABlendEnabled", "true", xml);
+    write_element("ssroSampleCount", "2", xml);
+    write_element("snowGlints", "true", xml);
+    write_text_element("POMQuality", "kSettingLevel_Ultra", xml);
+    write_element("probeRelightEveryFrame", "false", xml);
+    write_text_element("scalingMode", "kSettingScale_Mode1o1", xml);
+    write_element("reflectionMSAA", "0", xml);
+    write_element("lodScale", "1.0", xml);
+    write_element("grassLod", "3.0", xml);
+    write_element("pedLodBias", "0", xml);
+    write_element("vehicleLodBias", "0", xml);
+    write_element("sharpenIntensity", "1", xml);
+    write_element("sharpenIntensity", "1", xml);
+    write_text_element("treeQuality", "kSettingLevel_Ultra", xml);
+    write_text_element("deepsurfaceQuality", "kSettingLevel_High", xml);
+    write_element("treeTessellationEnabled", "false", xml);
+}
+
+fn write_element(name: &str, val: &str, xml: &mut XmlWriter) {
+    xml.start_element(name);
+    xml.write_attribute("value", val);
+    xml.end_element();
+}
+
+fn write_text_element(name: &str, text: &str, xml: &mut XmlWriter) {
+    xml.start_element(name);
+    xml.write_text(text);
+    xml.end_element();
 }
